044b489fd1b22f76664ae6ce0c31194394490f89
rntgmml <rntgmml@gmail.com>
2023-07-08 20:15:04 -0300

    Add color pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,16 @@ module.exports = {
         sans: ['var(--font-codec)']
       }
     },
+    colors: {
+      darkGrey: '#121212',
+      white: '#FFFFFF',
+      whiteSmoke: '#F8F8F8',
+      grey: '#EDEDF1',
+      foggyGrey: '#ACA99F',
+      sonicSilver: '#777777',
+      warning: '#D24340',
+      lightYellow: '#FBC81D',
+    }
   },
   plugins: [],
 }
