13bc4d58a2946e188d45e619966f3b673bf32c72
rntgmml <rntgmml@gmail.com>
2023-07-09 22:44:59 -0300

    Change input to have mobile number

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,81 @@
+'use client'
+import { ChangeEvent, useEffect, useState } from "react"
 import Button from "@/components/button"
+import Header from "@/components/header"
 import Input from "@/components/input"
+import { isEmail } from "@/utils/isEmailValid"
+import { isFullName } from "@/utils/isFullNameValid"
+import { isPossiblePhoneNumber } from 'react-phone-number-input'
+
 
 export default function Home() {
+  const [name, setName] = useState('');
+  const [isNameValid, setIsNameValid] = useState(false);
+  const [email, setEmail] = useState('');
+  const [isEmailValid, setIsEmailValid] = useState(false);
+  const [mobileNumber, setMobileNumber] = useState('')
+  const [isMobileValid, setIsMobileValid] = useState(false);
+  const [password, setPassword] = useState('');
+  const [isPasswordValid, setIsPasswordValid] = useState(false);
+  const [step, setStep] = useState(0);
+
+
+  useEffect(() => {
+    const validStatesCount = [isNameValid, isEmailValid, isMobileValid, isPasswordValid].filter(
+      isValid => isValid
+    ).length;
+    setStep(validStatesCount)
+  }, [isNameValid, isEmailValid, isMobileValid, isPasswordValid]);
+
+  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value)
+    if (isFullName(name)) {
+      setIsNameValid(true)
+    } else {
+      setIsNameValid(false)
+    }
+  }
+
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value)
+    if (isEmail(email)) {
+      setIsEmailValid(true)
+    } else {
+      setIsEmailValid(false)
+    }
+  }
+
+  const handleMobileChange = (newValue: any) => {
+    setMobileNumber(newValue)
+    if (mobileNumber !== undefined) {
+      setIsMobileValid(true)
+    } else {
+      setIsMobileValid(false)
+    }
+  };
+
+  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value)
+    if (password.length > 4) {
+      setIsPasswordValid(true)
+    } else {
+      setIsPasswordValid(false)
+    }
+  }
+
   return (
-    <div className="p-5">
-      <h1 className='font-semibold text-2xl text-darkGrey'>Join VerifyMyAge</h1>
-      <Input legend="Full Name" />
+    <div className="px-6 py-5">
+      <Header step={step} />
+      <Input value={name} onChange={handleNameChange} type="text" legend="Full Name" isValid={isNameValid || name === ''} />
+      <Input value={email} onChange={handleEmailChange} type="text" legend="Email" isValid={isEmailValid || email === ''} />
+      <Input legend="Mobile" isMobile onChange={handleMobileChange} isValid={isMobileValid || mobileNumber === undefined} />
+      <Input value={password} onChange={handlePasswordChange} type="password" legend="Password" isPassword isValid={isPasswordValid || password === ''} />
       <Button variant="primary">
-        Primary Button
+        Save
       </Button>
       <Button variant="secondary">
-        Secondary Button
+        Got a VerifyMyAge account? Sign in
       </Button>
-      <Button
-        variant="primary"
-      >
-        Icon Button
-      </Button>
-    </div >
+    </div>
   )
 }
--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,15 +1,51 @@
-import React from 'react';
-
+import React, { ChangeEvent, useEffect, useState } from 'react';
+import PasswordIcon from '../../../public/images/password.svg'
+import Image from 'next/image';
+import 'react-phone-number-input/style.css'
+import PhoneInput from 'react-phone-number-input'
 
 type InputProps = {
     legend: string;
+    type?: string;
+    value?: string;
+    onChange?: (e: ChangeEvent<HTMLInputElement>) => void
+    isValid?: boolean;
+    isPassword?: boolean;
+    isMobile?: boolean
+    mobileNumber?: number
 };
 
-export const Input = ({ legend }: InputProps) => {
+export const Input = ({ legend, isValid = true, isPassword = false, type, value, onChange, isMobile = false, mobileNumber }: InputProps) => {
+    const borderClassName = isValid ? 'border-sonicSilver' : 'border-warning';
+    const [valueMobile, setValueMobile] = useState<any>(mobileNumber);
+
+    useEffect(() => {
+        if (isMobile) {
+            onChange && onChange(valueMobile);
+        }
+    }, [valueMobile, isMobile, onChange]);
+
+    const handleMobileChange = (newValue: any) => {
+        setValueMobile(newValue);
+    };
+
     return (
-        <fieldset className="border border-solid border-sonicSilver py-2 px-2 rounded-full w-full sm:w-[500px]">
+        <fieldset className={`border border-solid ${borderClassName} py-2 px-2 rounded-full w-full md:w-[500px] mb-5`}>
             <legend className="text-sm font-semibold px-1.5">{legend}</legend>
-            <input placeholder="Please type in..." className="ml-2 focus:outline-none" />
+            <div className="relative flex items-center">
+                {isMobile ? (
+                    <PhoneInput placeholder="Enter phone number" value={valueMobile} onChange={handleMobileChange} />
+
+                ) : (
+                    <input type={type} placeholder="Please type in..." className="ml-2 focus:outline-none" value={value} onChange={onChange} />
+
+                )}
+                {isPassword && (
+                    <div className="absolute right-1">
+                        <Image src={PasswordIcon} priority alt="Password icon" className="mr-2" width={16} height={16} />
+                    </div>
+                )}
+            </div>
         </fieldset>
     )
 }
